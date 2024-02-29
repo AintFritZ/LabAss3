6a9d9edf2092983eabaec420bbef116425912d38
Eti <114860898+AintFritZ@users.noreply.github.com>
2024-02-29 12:28:27 +0000

    LabAss3

--- PaymentApp.java
+++ PaymentApp.java
@@ -3,26 +3,29 @@ public class PaymentApp {
     public static void main(String[] args){
 
         Order order = new Order("keyboard", 300.0, 10, new GCash());
-        System.out.println("Order item is " + order.getItem());
-        System.out.println("Unit price is " + order.getUnitPrice());
-        System.out.println("Quantity is " + order.getQuantity());
 
-        System.out.println("\nPayment order details if " + order.getMode().getClass().getSimpleName());
-        System.out.println("Discount rate is " + order.getMode().determineDiscountRate());
-        System.out.println("Payment amount is " + order.getTotalAmount());
+        System.out.println("Order item is " + order.getItem() +
+                "\nUnit price is " + order.getUnitPrice() +
+                "\nQuantity is " + order.getQuantity());
+
+        PaymentMode mode = order.getMode();
+        System.out.println("\nPayment order details if " + mode.getClass().getSimpleName() +
+                "\nDiscount rate is " + mode.determineDiscountRate() +
+                "\nPayment amount is " + order.getTotalAmount());
 
         order.setMode(new Maya());
         order.setTotalAmount();
-        System.out.println("\nPayment order details if " + order.getMode().getClass().getSimpleName());
-        System.out.println("Discount rate is " + order.getMode().determineDiscountRate());
-        System.out.println("Payment amount is " + order.getTotalAmount());
+        mode = order.getMode();
+        System.out.println("\nPayment order details if " + mode.getClass().getSimpleName() +
+                "\nDiscount rate is " + mode.determineDiscountRate() +
+                "\nPayment amount is " + order.getTotalAmount());
 
         order.setMode(new ShopeePay());
         order.setTotalAmount();
-        System.out.println("\nPayment order details if " + order.getMode().getClass().getSimpleName());
-        System.out.println("Discount rate is " + order.getMode().determineDiscountRate());
-        System.out.println("Payment amount is " + order.getTotalAmount());
-
+        mode = order.getMode();
+        System.out.println("\nPayment order details if " + mode.getClass().getSimpleName() +
+                "\nDiscount rate is " + mode.determineDiscountRate() +
+                "\nPayment amount is " + order.getTotalAmount());
     }
 
 }
